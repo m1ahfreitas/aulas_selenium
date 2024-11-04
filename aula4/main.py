570beb76d13d6309695103a384e0cd8f63e9adf2
m1ahfreitas <maiara.freitas@alunos.ifsuldeminas.edu.br>
2024-11-04 08:43:20 -0300

    aula4

--- aula4/main.py
+++ aula4/main.py
@@ -2,48 +2,69 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 import pandas as pd
+import wget
 
 #cria um objeto para manipular o navegador
 driver = webdriver.Chrome()
 
-#abre um endereço no navegador
-driver.get("https://sg.portal-pokemon.com/play/pokedex/0001")
+#cria uma planilha para salvar os dados do pokemon
+planilha= pd.DataFrame(columns=['Numero', 'Nome', 'Altura', 'Categoria', 'Peso', 'Tipo', 'Image'])
 
-#seleciona o elemento que possui o número do pokemon
-elementNumero = driver.find_element(By.CLASS_NAME, "pokemon-slider__main-no")
+#cria um laço de repetição para saçlvar dados de mais pokemons
+for numero in range(1, 10):
+    codigoPoke = str(numero).zfill(4)
 
-#seleciona o elemento que possui o nome do pokemon
-elementNome = driver.find_element(By.CLASS_NAME, "pokemon-slider__main-name")
-
-#seleciona o elemento que possui o tipo do pokemon
-elementsTipo = driver.find_elements(By.CSS_SELECTOR, ".pokemon-type a>span")
-
-#seleciona o elemento que possui o altura do pokemon
-elementAltura = driver.find_element(By.CSS_SELECTOR, ".pokemon-info__height .pokemon-info__value")
-
-#seleciona o elemento que possui o categoria do pokemon
-elementCategoria = driver.find_element(By.CSS_SELECTOR, ".pokemon-info__category .pokemon-info__value")
-
-#seleciona o elemento que possui o peso do pokemon
-elementPeso = driver.find_element(By.CSS_SELECTOR, ".pokemon-info__weight .pokemon-info__value")
+    #abre um endereço no navegador
+    driver.get("https://sg.portal-pokemon.com/play/pokedex/"+codigoPoke)
 
 
-print("Número: ", elementNumero.text)
-print("Nome: ", elementNome.text)
-print("Altura: ", elementAltura.text)
-print("Categoria: ", elementCategoria.text)
-print("Peso: ", elementPeso.text)
-for tipo in elementsTipo:
-    print("Tipo: ", tipo.text)
+
+    #seleciona o elemento que possui o número do pokemon
+    elementNumero = driver.find_element(By.CLASS_NAME, "pokemon-slider__main-no")
+
+    #seleciona o elemento que possui o nome do pokemon
+    elementNome = driver.find_element(By.CLASS_NAME, "pokemon-slider__main-name")
+
+    #seleciona o elemento que possui o tipo do pokemon
+    elementsTipo = driver.find_elements(By.CSS_SELECTOR, ".pokemon-type a>span")
+
+    #seleciona o elemento que possui o altura do pokemon
+    elementAltura = driver.find_element(By.CSS_SELECTOR, ".pokemon-info__height .pokemon-info__value")
+
+    #seleciona o elemento que possui o categoria do pokemon
+    elementCategoria = driver.find_element(By.CSS_SELECTOR, ".pokemon-info__category .pokemon-info__value")
+
+    #seleciona o elemento que possui o peso do pokemon
+    elementPeso = driver.find_element(By.CSS_SELECTOR, ".pokemon-info__weight .pokemon-info__value")
+    
+    #seleciona o elemento image
+    elementImage = driver.find_element(By.CLASS_NAME, "pokemon-img__front")
+    urlOrigin = elementImage.get_attribute('src')
+    extensao = urlOrigin.split(".")[-1]
+    urlDestino = "aula4\\img\\"+codigoPoke +  "." + extensao
+    #faz o donload da imagem
+    wget.donload(urlOrigin, urlDestino)
 
 
-df = pd.DataFrame(columns=['Numero', 'Nome'])
-df.loc[1] = [elementNumero.text, elementNome.text]
-df.to_excel('planilha.xlsx')
+    print("Número: ", elementNumero.text)
+    print("Nome: ", elementNome.text)
+    print("Altura: ", elementAltura.text)
+    print("Categoria: ", elementCategoria.text)
+    print("Peso: ", elementPeso.text)
 
-import os
 
-os.path.splitext()[1]
+    tipos = ""
+    for tipo in elementsTipo:
+        print("Tipo: ", tipo.text)
+        tipos = tipo.text +  ", " + tipos
+
+
+    #planilha= pd.DataFrame(columns=['Numero', 'Nome', 'Altura', 'Categoria', 'Peso', 'Tipo'])
+    planilha.loc[numero-1]= [elementNumero.text, elementNome.text, elementAltura.text, elementCategoria.text, elementPeso.text, tipos, urlDestino]
+
+planilha.to_excel('pokedex.xlsx', index = False)
+
+
 
 #impede o fechamento do navegador
 input("Digite algo para terminar")
